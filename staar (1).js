ff08979a6a79dee4f8f858571533ad3bceec9dc9
Steph Dunn <stephanie@Stephanies-MacBook-Pro.local>
2016-06-06 15:32:06 +0100

    Initial Commit

---
+++ staar (1).js
@@ -0,0 +1,52 @@
+// accordion function for indigo highlights (state of the art section), adapted from http://www.w3schools.com/w3css/w3css_accordions.asp 
+function accordIndigo(id) {
+    var x = document.getElementById(id);
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+        x.previousElementSibling.className += " w3-indigo";
+    } else { 
+        x.className = x.className.replace(" w3-show", "");
+        x.previousElementSibling.className = 
+        x.previousElementSibling.className.replace(" w3-indigo", "");
+    }
+}
+
+// accordion function for bluegrey highlights (prototype + technology section), adapted from http://www.w3schools.com/w3css/w3css_accordions.asp 
+function accordBlue(id) {
+    var x = document.getElementById(id);
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+        x.previousElementSibling.className += " w3-blue-grey";
+    } else { 
+        x.className = x.className.replace(" w3-show", "");
+        x.previousElementSibling.className = 
+        x.previousElementSibling.className.replace(" w3-blue-grey", "");
+    }
+}
+
+
+// slideshow (vision section), adapted from http://www.w3schools.com/w3css/w3css_slideshow.asp 
+var slideIndex = 1;
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+     dots[i].className = dots[i].className.replace(" w3-white", "");
+  }
+  x[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " w3-white";
+}
